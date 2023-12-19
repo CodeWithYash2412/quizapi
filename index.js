260a49d2ac797a540e0a59c0ab6be20762612aef
CodeWithYash2412 <84624661+CodeWithYash2412@users.noreply.github.com>
2023-12-19 14:09:12 +0530

    initial

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 const port = process.env.PORT || 5000;
 
 
